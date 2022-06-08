c09318c19969bf9a1e189a475c1513a925f01b2e
Nadia <dracken24@gmail.com>
2022-06-08 13:54:13 -0400

    ok

---
+++ lst/test_lst.c
@@ -0,0 +1,11 @@
+#include "../libft.h"
+
+int main(void)
+{
+    char *str = "allo";
+    t_list *lst0;
+    t_list *lst1;
+
+    ft_lstadd_front(&lst1, lst0);
+    // printf("lst 01: %s", lst1->str);
+}
